e4c61c9772aef1fca884a59f877cbd874242c283
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-20 13:41:35 +0300

    added dispatch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import store from "./store";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+console.log("initial state: ", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log(("State after dispatch: ", store.getState()))
+);
+//dispatch some actions
+store.dispatch({ type: "todos/todoAdded", payload: "Learn about actions" });
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+
+store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active'})
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: {color: 'red', changeType: 'added'}
+})
+
+//stop listening to state updates
+unsubscribe()
 
 root.render(
   <StrictMode>
